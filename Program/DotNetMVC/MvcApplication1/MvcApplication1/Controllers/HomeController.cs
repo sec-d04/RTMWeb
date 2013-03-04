bfb313a5a9a9fddd8f7b227728b9df431f998d49
MtBlue81 <mt.blue81@gmail.com>
2013-03-04 11:03:30 +0900

    MVCサンプル追加

---
+++ Program/DotNetMVC/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenRTM.Core;
+using OpenRTM.IIOP;
+
+namespace MvcApplication1.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewData["Message"] = "ASP.NET MVC へようこそ";
+            var manager = new Manager();
+            manager.AddTypes(typeof(CorbaProtocolManager));
+            manager.Activate();
+            var comp = manager.CreateComponent<ExampleComponent>();
+            manager.Run();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program/DotNetMVC/MvcApplication1/MvcApplication1/Views/Home/Index.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    ホーム ページ
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: ViewData["Message"] %></h2>
+    <p>
+        ASP.NET MVC の詳細については、<a href="http://asp.net/mvc" title="ASP.NET MVC Website">http://asp.net/mvc</a> を参照してください。
+    </p>
+</asp:Content>
